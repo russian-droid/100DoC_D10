d68492edd3ec569abfbcb3e14dd7318d3c49f756
Igor the roofCoder <53712180+russian-droid@users.noreply.github.com>
2021-03-27 02:46:32 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-#
+#following Udemy course: 100 days of code by Angela Yu
+
+#------------------------------days in month--------
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        #print("Leap year.")
+        return True
+      else:
+        #print("Not leap year.")
+        return False
+    else:
+      #print("Leap year.")
+      return True
+  else:
+    #print("Not leap year.")
+    return False
+
+def days_in_month(year, month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    days=month_days[month-1]
+    year=is_leap(year)
+
+    if year == True:
+        if month == 2:
+            days = 29
+        else: 
+            days=month_days[month-1]
+    else:
+        days
+    
+    return days
+  
+  
+#🚨 Do NOT change any of the code below 
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
