4fd388c2ed398006f427ab4d61e00f10fa15ed25
Igor the roofCoder <53712180+russian-droid@users.noreply.github.com>
2021-03-27 04:50:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -33,9 +33,65 @@ def days_in_month(year, month):
     
     return days
   
-  
 #🚨 Do NOT change any of the code below 
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
 days = days_in_month(year, month)
 print(days)
+
+
+#------------------------------days in month--------
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+print(logo)
+
+#add
+def add(n1, n2):
+  return n1 + n2
+#subtract
+def subtract(n1, n2):
+  return n1 - n2
+#multiply
+def multiply(n1, n2):
+  return n1 * n2
+#divide
+def divide(n1, n2):
+  return n1 / n2
+
+operations = {
+  '+':add, #make sure in this case values are not saved as strings!
+  '-':subtract,
+  '*':multiply,
+  '/':divide,
+}
+
+#function = operations['+']
+#function(2,3)
+
+num1 = int(input('First number:\n'))
+num2 = int(input('Second number:\n'))
+
+print('\nAvailable operations:')
+for symbol in operations:
+    print (symbol)
+
+operation_symbol = input('Enter an operation from above:\n')
+calc_function = operations[operation_symbol]
+answer = calc_function(num1, num2)
+
+print(f'{num1} {operation_symbol} {num2} = {answer}')
+
